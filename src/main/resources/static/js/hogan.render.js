8eb2ee407f4116167b4d05eb1727f054901dd094
Zakhar Kliap <Zakhar_Kliap@epam.com>
2016-04-27 16:44:31 +0300

    fix hogan displaying #2

--- src/main/resources/static/js/hogan.render.js
+++ src/main/resources/static/js/hogan.render.js
@@ -7,8 +7,7 @@ var hoganRenderer = {
 		$.get(window.location.pathname + '/content/', function(templateRaw) {
 			var template = Hogan.compile(templateRaw);
 			$.get(window.location.pathname + '/data', function(data) {
-				$('.main-content').append(template.render({"data":data}));
-				console.log(template.render(data))
+				$('.main-content').append(template.render({"original":data}));
 			});
 		});
 	},
